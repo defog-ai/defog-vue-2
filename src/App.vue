8b6612e03f86f96c22f8e1589607228c41f2be00
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-10-10 11:50:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Defog
+    maxWidth="100%"
+    maxHeight="100%"
+    apiEndpoint="https://test-defog-ikcpfh5tva-uc.a.run.app"
+    buttonText="Ask Defog"
+    darkMode=false
+  />
+</template>
+
+<script>
+import { applyReactInVue } from 'vuereact-combined'
+import { AskDefogChat } from "defog-components"
+export default {
+  components: {
+    // Use HOC 'applyReactInVue'
+    Defog: applyReactInVue(AskDefogChat)
+  }
+}
+</script>
